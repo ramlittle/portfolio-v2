4db4d183cc2a9cf26810d48a730dc4f7618bfcbf
ramelitomartinez <ramelitomartinez@gmail.com>
2023-05-17 15:44:18 -0400

    designed menu and finished js

---
+++ js/menu.js
@@ -0,0 +1,33 @@
+const MenuList = document.querySelector('.menu-list-container');
+
+const url = '../data/menu.json';
+let menuList=[];
+
+function fetchData(){
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>{
+       displayData(data);
+    })
+}
+
+function displayData(data){
+    if(!data.length){
+        return Header.insertAdjacentHTML('beforeend',`
+            <a href = ''>No Menu List Found</a>
+        `)
+    }
+    //display menu list here
+    for(let i=0;i<data.length;i++){
+        MenuList.insertAdjacentHTML('beforeend',`
+            <a href='${data[i].link}'>${data[i].title}</a>
+        `)
+    }
+}
+
+//Obtain Menu and Display
+fetchData();
+
+
+
+
