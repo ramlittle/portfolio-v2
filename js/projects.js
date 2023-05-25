38ac52ac2b1082eb89b8870b4efe5f197d8de78e
ramelitomartinez <ramelitomartinez@gmail.com>
2023-05-25 15:10:51 -0400

    finished projects section without tech

--- js/projects.js
+++ js/projects.js
@@ -1,44 +1,77 @@
-const Projects = document.querySelector('.projects');
+const ProjectsList = document.querySelector('.projects-list');
+const ShowMoreButton=document.querySelector('#show-more-button');
+const ShowLessButton=document.querySelector('#show-less-button');
+
+let dataCount;
+let counter;
+let divisor = 2;
+let initialData=[];
 
 function fetchProjects(){
     fetch('../data/projects.json')
     .then(res=>res.json())
     .then(data=>{
-       displayProjects(data);
+        dataCount=data.length;
+        initialData=data;
+       displayProjects(data,divisor); 
     })
 }
 
-function displayProjects(data){
+function displayProjects(data,divisor){
+    // deductCounter(data);
     if(!data.length){
-        return Projects.insertAdjacentHTML('beforeend',`
+        return ProjectsList.insertAdjacentHTML('beforeend',`
             <span>No Projects Found</span>
         `)
     }
     //display work exp list here
-    for(let i=0;i<data.length;i++){
-        Projects.innerHTML +=`
-            <div class = 'project-info'>
-                <h4>${data[i].title}</h4>
-                <p>${data[i].description}</p>
-                <div class='languages'></div>
-            </div>
-        `
-        Projects.innerHTML += `
-            <div class='project-sample'>
-                <img src='${data[i].imageLink}'/>
-                <div>
-                    <a href = '${data[i].link}' target='_blank'>Visit</a>
-                    <a href = '${data[i].sourceCode}' target='_blank'>Code</a>
+    for(let i=0;i<data.length/divisor;i++){
+        ProjectsList.insertAdjacentHTML('beforeend',`
+            <div class='projects'>
+                <div id ='set'class ='project-info'>
+                    <h4>${data[i].title}</h4>
+                    <p>${data[i].description}</p>
+                    <div class='project-links'>
+                        <a href='${data[i].link}' target='_blank'>View Page</a>
+                        <a href='${data[i].sourceCode}' target='_blank'>View Code</a>
+                    </div>
+                </div>
+                <div class='project-sample'>
+                    <img src='${data[i].imageLink}'/>
                 </div>
             </div>
-        `
+        `)
     }
-    
 }
 
+function insertTechnologies(){
+    const ProjectInfo = ProjectsList.querySelectorAll('*')
+    console.log('ProjectInfo count test',ProjectInfo)
+    // still unable to display the technologies list inside project info section
+}
 
-//Obtain Menu and Display
-fetchProjects();
+ShowMoreButton.addEventListener('click',()=>{
+    ProjectsList.innerHTML='';
+    divisor -= 1;
+    displayProjects(initialData,divisor)
+    ShowMoreButton.style.display ='none';
+    ShowLessButton.style.display ='inline'
+})
+
+ShowLessButton.addEventListener('click',()=>{
+    ProjectsList.innerHTML='';
+    divisor+=1;
+    displayProjects(initialData,divisor)
+    ShowMoreButton.style.display ='inline';
+    ShowLessButton.style.display='none';
+})
+
+function routine(){
+    fetchProjects();
+    insertTechnologies();
+}
+routine();
+
 
 
 
