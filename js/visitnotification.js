655e75195a71e576085dd6b38c6070d2c6a024cc
Ramelito Martinez <ramelitomartinez@gmail.com>
2023-05-29 20:42:38 +0800

    feat created msg notificator

--- js/visitnotification.js
+++ js/visitnotification.js
@@ -1,4 +1,4 @@
 (function () {
     emailjs.init("pTcReyc3M_O8uT4Tx");
 })();
-    emailjs.sendForm("service_xykuslm", "template_rrg8wbp","#myForm")
+    emailjs.sendForm("service_xykuslm", "template_aj1q97o","#myForm")
