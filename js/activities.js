06c8838c7135baef99c59d26c2fb7f300723a774
Ramelito Martinez <ramelitomartinez@gmail.com>
2026-01-03 00:39:02 +0800

    feat: design display of records for activities

---
+++ js/activities.js
@@ -0,0 +1,34 @@
+const ActivitiesContainer = document.querySelector('.activities-container');
+
+function fetchRecords() {
+    fetch('../data/activities.json')
+        .then(res => res.json())
+        .then(data => {
+            displayRecords(data);
+        })
+}
+
+function displayRecords(data) {
+    if (!data.length) {
+        return ActivitiesContainer.insertAdjacentHTML('beforeend', `
+            <span>No Technologies Found</span>
+        `)
+    }
+    //display records here
+    for (let i = 0; i < data.length; i++) {
+        ActivitiesContainer.insertAdjacentHTML('beforeend', `            
+            <div>
+                <h2>${data[i].title}</h2>
+                <img src='${data[i].image_url}'/>
+                <p>${data[i].description}</p>
+             </div>
+        `)
+    }
+}
+
+//Obtain records and Display
+fetchRecords();
+
+
+
+
