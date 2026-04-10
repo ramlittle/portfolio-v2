ad2672f429b7e18d7043e521d154ad26438705c5
Ramelito Martinez <ramelitomartinez@gmail.com>
2026-04-11 07:42:34 +0800

    feat: added record counter

--- pages/clients/rog/js/landing_pages.js
+++ pages/clients/rog/js/landing_pages.js
@@ -3,13 +3,17 @@ function obtainRecords(searchQuery) {
     fetch('/pages/clients/rog/data/landing_pages.json')
         .then(res => res.json())
         .then(data => {
-            const filteredData = data.filter(obj => obj.title.toLowerCase().includes(searchQuery.toLowerCase()));            
+            const filteredData = data.filter(obj => obj.title.toLowerCase().includes(searchQuery.toLowerCase()));
             displayRecords(filteredData);
         })
 }
 
 function displayRecords(data) {
+    const heroSection = document.querySelector('#hero-section');
+    const recordCountSpan = document.querySelector('.record-count-span')
     const landingPagesContainer = document.querySelector('#landing-pages-container');
+    const count = data.length;    
+
     landingPagesContainer.innerHTML = '';
     if (!data.length) {
         return landingPagesContainer.insertAdjacentHTML('beforeend', `
@@ -18,11 +22,12 @@ function displayRecords(data) {
             </div
         `)
     }
+
     //display records here
     for (let i = 0; i < data.length; i++) {
         const imgLink = data[i].imageLink;
         const link = data[i].link;
-        landingPagesContainer.insertAdjacentHTML('beforeend', `
+        landingPagesContainer.insertAdjacentHTML('beforeend', `           
             <div>
                 <img src="${imgLink}"/>
                 <h3>${data[i].title}</h3>
@@ -31,9 +36,18 @@ function displayRecords(data) {
             </div>
         `)
     }
+
+    //display record count
+    if(recordCountSpan){
+        recordCountSpan.remove();
+    }else{
+        heroSection.insertAdjacentHTML('beforeend', `
+        <span class="record-count-span" style="color:white;">Showing ${count} records</span>`);
+    }
+    
 }
 
-function searchRecord(searchQuery) {    
+function searchRecord(searchQuery) {
     obtainRecords(searchQuery);
 }
 
